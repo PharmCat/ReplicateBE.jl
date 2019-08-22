d264092ed048b38499f537b907f8871f632b8fcf
PharmCat <v.s.arnautov@yandex.ru>
2019-08-22 19:01:06 +0300

    v0.1.1

--- src/ReplicateBE.jl
+++ src/ReplicateBE.jl
@@ -9,80 +9,86 @@ using DataFrames, Distributions, StatsModels, ForwardDiff, LinearAlgebra, Optim
 
     export rbe
     import Base.show
+    import Statistics.var
 
 struct RBE
-    model
-    factors
-    β
-    θ
-    reml
-    se
-    F
-    DF
-    R
-    V
-    G
-    A
-    H
-    detH
+    model::ModelFrame
+    factors::Array{Symbol, 1}
+    β::Array{Float64, 1}
+    θ::Array{Float64, 1}
+    reml::Float64
+    se::Array{Float64, 1}
+    F::Array{Float64, 1}
+    DF::Array{Float64, 1}
+    R::Array{Matrix{Float64},1}
+    V::Array{Matrix{Float64},1}
+    G::Matrix{Float64}
+    A::Matrix{Float64}
+    H::Matrix{Float64}
+    detH::Float64
+    optim::Optim.MultivariateOptimizationResults
 end
 
-#=
-function Base.show(obj::RBE)
-    obj.model.contrasts
-    for f in obj.factors
-    end
-end
-=#
-function rbe(df; var::Symbol, subject::Symbol, formulation::Symbol, period::Symbol, sequence::Symbol)
+include("show.jl")
+
+"""
+    Mixed model fitting function
+"""
+function rbe(df; dvar::Symbol,
+    subject::Symbol,
+    formulation::Symbol,
+    period::Symbol,
+    sequence::Symbol,
+    g_tol::Float64 = 1e-8, x_tol::Float64 = 0.0, f_tol::Float64 = 0.0)
+
     categorical!(df, subject);
     categorical!(df, formulation);
     categorical!(df, period);
     categorical!(df, sequence);
     sort!(df, [subject, formulation, period])
-    Xf = @eval(@formula($var ~ $sequence + $period + $formulation))
-    Zf = @eval(@formula($var ~ 0 + $formulation))
+    Xf = @eval(@formula($dvar ~ $sequence + $period + $formulation))
+    Zf = @eval(@formula($dvar ~ 0 + $formulation))
     MF = ModelFrame(Xf, df)
     X  = ModelMatrix(MF).m
     Z  = ModelMatrix(ModelFrame(Zf, df, contrasts = Dict(formulation => StatsModels.FullDummyCoding()))).m
-
+    p  = rank(X)
     if size(Z)[2] != 2 error("Size random effect matrix != 2. Not implemented yet!") end
 
-    y = df[:, var]
+    y = df[:, dvar]
     Xv, Zv, yv = sortsubjects(df, subject, X, Z, y)
     checkdata(X, Z, Xv, Zv, y)
 
-    θvec0 = [0.02, 0.02, 0.2, 0.2, 0.001]
+    iv = initvar(df, dvar, formulation, subject)
+    if iv[1] < iv[3] || iv[2] < iv[3] iv[1] = iv[2] = 2*iv[3] end
+    θvec0 = [iv[3], iv[3], iv[1]-iv[3], iv[2]-iv[3], 0.001]
 
-    remlf(x) = -2*reml(yv, Zv, X, Xv, x)
+    remlf(x) = -reml2(yv, Zv, p, Xv, x)
 
-    O  = optimize(remlf, θvec0, method=Newton(),  g_tol= 1e-12, x_tol = 1e-12, store_trace = true, extended_trace = true, show_trace = false)
+    O  = optimize(remlf, θvec0, method=Newton(),  g_tol=g_tol, x_tol=x_tol, f_tol=f_tol, allow_f_increases = true, store_trace = true, extended_trace = true, show_trace = false)
     θ  = Optim.minimizer(O)
     H  = Optim.trace(O)[end].metadata["h(x)"]
-    dH = det(H)
-    H[5,:] .= 0
-    H[:,5] .= 0
     remlv = remlf(θ)
     G     = gmat(θ[3], θ[4], θ[5])
     Rv    = rmatvec(θ[1], θ[2], Zv)
     Vv    = vmatvec(Zv, G, Rv)
     iVv   = inv.(Vv)
-
     β     = βcoef(yv, X, Xv, iVv)
-    #=
-    L    = [0 0 0 0 0 1]
-    C    = cmat(Xv, Zv, θ)
-    se   = sqrt((L*C*L')[1])
-    F    = (L*β)'*inv(L*C*L')*(L*β)
-    lclg(x) = lcgf(L, Xv, Zv, x)
-    g       = ForwardDiff.gradient(lclg, θ)
-    df      = 2*((L*C*L')[1])^2/(g'*(2*pinv(H))*g)
-    =#
+
+    if θ[5] > 1 θ[5] = 1 end
+    remlf2(x) = -2*reml(yv, Zv, p, Xv, x, β)
+    H         = ForwardDiff.hessian(remlf2, θ)
+    dH        = det(H)
+    H[5,:] .= 0
+    H[:,5] .= 0
+
     A         = 2*pinv(H)
-    se, F, df = ctrst(X, Xv, Zv, θ, β, A)
-    return RBE(MF, [sequence, period, formulation], β, θ, remlv, se, F, df, Rv, Vv, G, A, H, dH)
+    se, F, df = ctrst(p, Xv, Zv, θ, β, A)
+    return RBE(MF, [sequence, period, formulation], β, θ, remlv, se, F, df, Rv, Vv, G, A, H, dH, O)
 end
 
+"""
+    Make X, Z mtrices and vector y for each subject;
+"""
 function sortsubjects(df::DataFrame, sbj::Symbol, X::Matrix, Z::Matrix, y::Vector)
     u = unique(df[:, sbj])
     Xa = Array{Array{Float64,2}, 1}(undef, 0)
@@ -104,7 +110,9 @@ function sortsubjects(df::DataFrame, sbj::Symbol, X::Matrix, Z::Matrix, y::Vecto
     end
     return Xa, Za, ya
 end
-
+"""
+    G matrix
+"""
 function gmat(σ₁, σ₂, ρ)
     if ρ > 1.0 ρ = 1.0 end
     if σ₁ < 0.0 σ₁ = 1.0e-6 end
@@ -112,13 +120,17 @@ function gmat(σ₁, σ₂, ρ)
     cov = sqrt(σ₁ * σ₂) * ρ
     return [σ₁ cov; cov σ₂]
 end
-#=
-function rmat(σ, Z)
+
+"""
+    R matrix (ForwardDiff+)
+"""
+@inline function rmat(σ, Z)
     if σ[1] < 0.0 σ[1] = 1.0e-6 end
     if σ[2] < 0.0 σ[2] = 1.0e-6 end
     return Matrix(Diagonal((Z*σ)[:,1]))
 end
-=#
+
+#=
 function rmat(σ₁, σ₂, Z)
     if σ₁ < 0.0 σ₁ = 1.0e-6 end
     if σ₂ < 0.0 σ₂ = 1.0e-6 end
@@ -128,11 +140,17 @@ function rmat(σ₁, σ₂, Z)
     end
     return M
 end
+=#
+#=
 function rmat2(σ, Z)
     if σ[1] < 0.0 σ[1] = 1.0e-6 end
     if σ[2] < 0.0 σ[2] = 1.0e-6 end
     return Matrix(Diagonal((Z*σ)[:,1]))
 end
+=#
+"""
+    Return variance-covariance matrix V
+"""
 function cov(G, R, Z)
     V  = Z*G*Z' + R
 end
@@ -140,7 +158,7 @@ function rmatvec(σ₁, σ₂, Zvec)
     n  = length(Zvec)
     Ra = Array{Array{Float64,2}, 1}(undef, n)
     for i = 1:n
-        Ra[i] = rmat(σ₁, σ₂, Zvec[i])
+        Ra[i] = rmat([σ₁, σ₂], Zvec[i])
     end
     return Ra
 end
@@ -174,6 +192,7 @@ function βcoef(yv, X, Xv, iVv)
     end
     return inv(A)*β
 end
+#=
 function reml(yv, Zv, X, Xv, θvec)
     n = length(yv)
     N = sum(length.(yv))
@@ -198,19 +217,41 @@ function reml(yv, Zv, X, Xv, θvec)
     #println("θ₁: ", θ1, " θ₂: ",  θ2,  " θ₃: ", θ3)
     return   -(θ1/2 + θ2/2 + θ3/2 + c)
 end
+=#
+function reml(yv, Zv, p, Xv, θvec, β)
+    n = length(yv)
+    N = sum(length.(yv))
+    G = gmat(θvec[3], θvec[4], θvec[5])
+    c  = (N-p)/2*log(2π)
+    θ1 = 0
+    θ2 = 0
+    θ3 = 0
+    iV   = nothing
+    θ2m  = zeros(p,p)
+    for i = 1:n
+        R   = rmat([θvec[1], θvec[2]], Zv[i])
+        V   = cov(G, R, Zv[i])
+        iV  = inv(V)
+        θ1  += logdet(V)
+        θ2m += Xv[i]'*iV *Xv[i]
+        r    = yv[i]-Xv[i]*β
+        θ3  += r'*iV*r
+    end
+    θ2       = logdet(θ2m)
+    return   -(θ1/2 + θ2/2 + θ3/2 + c)
+end
 function lcgf(L, Xv, Zv, θ)
     p   = size(Xv[1])[2]
     C   = zeros(p,p)
     G   = gmat(θ[3], θ[4], θ[5])
     for i=1:length(Xv)
-        R   = rmat2([θ[1], θ[2]], Zv[i])
+        R   = rmat([θ[1], θ[2]], Zv[i])
         iVv = inv(cov(G, R, Zv[i]))
         C   = C + Xv[i]'*iVv*Xv[i]
     end
     return (L*inv(C)*L')[1]
 end
-function ctrst(X, Xv, Zv, θ, β, A)
-    p     = rank(X)
+function ctrst(p, Xv, Zv, θ, β, A)
     C     = cmat(Xv, Zv, θ)
     se    = Array{Float64, 1}(undef, 0)
     F     = Array{Float64, 1}(undef, 0)
@@ -236,6 +277,76 @@ function checkdata(X, Z, Xv, Zv, y)
         if sum(Zv[i][:,1]) != 2 error("Subject $i, formulation 1, not have 2 observation, other solutions not implemented yet!!!") end
         if sum(Zv[i][:,2]) != 2 error("Subject $i, formulation 2, not have 2 observation, other solutions not implemented yet!!!") end
     end
+end
+
+
+#-------------------------------------------------------------------------------
+function reml2(yv, Zv, p, Xv, θvec)
+    n = length(yv)
+    N = sum(length.(yv))
+    G = gmat(θvec[3], θvec[4], θvec[5])
+    c  = (N-p)*log(2π)
+    θ1 = 0
+    θ2 = 0
+    θ3 = 0
+    iV   = nothing
+    θ2m  = zeros(p,p)
+    βm   = zeros(p)
+    for i = 1:n
+        R   = rmat([θvec[1], θvec[2]], Zv[i])
+        V   = cov(G, R, Zv[i])
+        iV  = inv(V)
+        θ1  += logdet(V)
+        tm   = Xv[i]'*iV    #Temp matrix for Xv[i]'*iV*Xv[i] and Xv[i]'*iV*yv[i] calc
+        θ2m += tm*Xv[i]
+        βm  += tm*yv[i]
+    end
+    β        = inv(θ2m)*βm
+    for i = 1:n
+        r    = yv[i]-Xv[i]*β
+        θ3  += r'*iV*r
+    end
+    θ2       = logdet(θ2m)
+    return   -(θ1 + θ2 + θ3 + c)
+end
+#=
+function βcoef2(yv, p, Xv, Zv, θvec)
+    n   = length(yv)
+    A   = zeros(p,p)
+    β   = zeros(p)
+    G   = gmat(θvec[3], θvec[4], θvec[5])
+    for i = 1:n
+        R   = rmat([θvec[1], θvec[2]], Zv[i])
+        iV = inv(cov(G, R, Zv[i]))
+        A = A + (Xv[i]'*iV*Xv[i])
+        β = β .+ Xv[i]'*iV*yv[i]
+    end
+    return inv(A)*β
+end
+=#
+function initvar(df, dv, fac, sbj)
+    u  = unique(df[:, sbj])
+    f  = unique(df[:, fac])
+    fv = Array{Float64, 1}(undef, 0)
+    for i in f
+        fvd = Array{Float64, 1}(undef, 0)
+        v = findall(x->x==i, df[:, fac])
+        for r in v
+            push!(fvd, df[r, dv])
+        end
+        push!(fv, var(fvd))
+    end
+    sv = Array{Float64, 1}(undef, 0)
+    for i in u
+        fvd = Array{Float64, 1}(undef, 0)
+        v   = findall(x->x==i, df[:, sbj])
+        for r in v
+            push!(fvd, df[r, dv])
+        end
+        push!(sv, var(fvd))
+    end
+    push!(fv, mean(sv))
+    return fv
 end
 
 end # module
--- test/test.jl
+++ test/test.jl
@@ -9,7 +9,7 @@ include("testdata.jl")
 
 @testset "  Mixed model test       " begin
     df = CSV.read(IOBuffer(minibe)) |> DataFrame
-    be = ReplicateBE.rbe(df, var = :var, subject = :subject, formulation = :formulation, period = :period, sequence = :sequence);
+    be = ReplicateBE.rbe(df, dvar = :var, subject = :subject, formulation = :formulation, period = :period, sequence = :sequence);
     @test be.β[6]  ≈  -0.0791666 atol=1E-6
     @test be.se[5] ≈   0.0903709 atol=1E-6
     @test be.reml  ≈  10.0652386 atol=1E-6

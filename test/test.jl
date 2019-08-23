a2557c1b70fc4874f9313bda0d1fae3ab606b1fd
PharmCat <mail@pharmcat.net>
2019-08-23 12:29:47 +0300

    Fix

--- test/test.jl
+++ test/test.jl
@@ -11,6 +11,6 @@ include("testdata.jl")
     df = CSV.read(IOBuffer(minibe)) |> DataFrame
     be = ReplicateBE.rbe(df, dvar = :var, subject = :subject, formulation = :formulation, period = :period, sequence = :sequence);
     @test be.β[6]  ≈  -0.0791666 atol=1E-6
-    @test be.se[5] ≈   0.0903709 atol=1E-6
+    @test be.se[6] ≈   0.0903709 atol=1E-6
     @test be.reml  ≈  10.0652386 atol=1E-6
 end

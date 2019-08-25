1d57e9058e962e5346c9ce3174af80e53bd585ed
PharmCat <v.s.arnautov@yandex.ru>
2019-08-25 23:53:00 +0300

    Major optimization

--- src/ReplicateBE.jl
+++ src/ReplicateBE.jl
@@ -61,17 +61,38 @@ function rbe(df; dvar::Symbol,
     X  = ModelMatrix(MF).m
     Z  = ModelMatrix(ModelFrame(Zf, df, contrasts = Dict(formulation => StatsModels.FullDummyCoding()))).m
     p  = rank(X)
-    if size(Z)[2] != 2 error("Size random effect matrix != 2. Not implemented yet!") end
-
-    y = df[:, dvar]
+    y  = df[:, dvar]
     Xv, Zv, yv = sortsubjects(df, subject, X, Z, y)
+    n  = length(Xv)
+    N  = sum(length.(yv))
+
+
+    if size(Z)[2] != 2 error("Size random effect matrix != 2. Not implemented yet!") end
     checkdata(X, Z, Xv, Zv, y)
+
+    qro   = qr(X)
+    β     = inv(qro.R)*qro.Q'*y
 
     iv = initvar(df, dvar, formulation, subject)
     if iv[1] < iv[3] || iv[2] < iv[3] iv[1] = iv[2] = 2*iv[3] end
     θvec0 = [iv[3], iv[3], iv[1]-iv[3], iv[2]-iv[3], 0.001]
 
-    remlf(x) = -reml2(yv, Zv, p, Xv, x)
+    G     = zeros(2, 2)
+    Rv    = Array{Array{Float64,2}, 1}(undef, n)
+    Vv    = Array{Array{Float64,2}, 1}(undef, n)
+    iVv   = Array{Array{Float64,2}, 1}(undef, n)
+    matvecz!(Rv, Zv)
+    matvecz!(Vv, Zv)
+    matvecz!(iVv, Zv)
+    #=
+    G     = gmat(θvec0[3], θvec0[4], θvec0[5])
+    Rv    = rmatvec(θvec0[1], θvec0[2], Zv)
+    Vv    = vmatvec(Zv, G, Rv)
+    iVv   = inv.(Vv)
+    =#
+
+    remlf(x) = -reml2!(yv, Zv, p, n, N, Xv, G, Rv, Vv, iVv, x, β)
+    #remlf(x) = -reml2(yv, Zv, p, Xv, x)
     method =LBFGS()
     #method=ConjugateGradient()
     #method = NelderMead()
@@ -79,22 +100,19 @@ function rbe(df; dvar::Symbol,
     #O  = optimize(remlf, θvec0, method=method,  g_tol=g_tol, x_tol=x_tol, f_tol=f_tol, iterations=iterations, allow_f_increases = true, store_trace = true, extended_trace = true, show_trace = false)
     pO = optimize(remlf, [limeps, limeps, limeps, limeps, limeps], [Inf, Inf, Inf, Inf, 1.0], θvec0, Fminbox(method), Optim.Options(g_tol = 1e-2))
     θ  = Optim.minimizer(pO)
+    remlf(x) = -reml2b!(yv, Zv, p, Xv, G, Rv, Vv, iVv, x, β)
+    #O  = optimize(remlf, θ, method=Newton(),  g_tol=g_tol, x_tol=x_tol, f_tol=f_tol, callback = βcoef!(p, n, yv, Xv, iVv, β), allow_f_increases = true, store_trace = true, extended_trace = true, show_trace = false)
     O  = optimize(remlf, θ, method=Newton(),  g_tol=g_tol, x_tol=x_tol, f_tol=f_tol, allow_f_increases = true, store_trace = true, extended_trace = true, show_trace = false)
     θ  = Optim.minimizer(O)
-    #=
-    if θ[5] > 1.0
-        θ[5] = 1.0
-        O  = optimize(remlf, θ, method=Newton(),  g_tol=g_tol, x_tol=x_tol, f_tol=f_tol, allow_f_increases = true, store_trace = true, extended_trace = true, show_trace = false)
-        θ  = Optim.minimizer(O)
-    end
-    =#
+
     #H  = Optim.trace(O)[end].metadata["h(x)"]
     remlv = remlf(θ)
-    G     = gmat(θ[3], θ[4], θ[5])
-    Rv    = rmatvec(θ[1], θ[2], Zv)
-    Vv    = vmatvec(Zv, G, Rv)
-    iVv   = inv.(Vv)
-    β     = βcoef(yv, X, Xv, iVv)
+    #G     = gmat(θ[3], θ[4], θ[5])
+    #Rv    = rmatvec(θ[1], θ[2], Zv)
+    #Vv    = vmatvec(Zv, G, Rv)
+    #iVv   = inv.(Vv)
+    #βcoef!(p, n, yv, Xv, iVv, β)
+    #β     = βcoef(yv, X, Xv, iVv)
     #
     if θ[5] > 1 θ[5] = 1 end
     remlf2(x) = -2*reml(yv, Zv, p, Xv, x, β)
@@ -104,7 +122,7 @@ function rbe(df; dvar::Symbol,
     H[:,5] .= 0
 
     A         = 2*pinv(H)
-    se, F, df = ctrst(p, Xv, Zv, θ, β, A)
+    se, F, df = ctrst(p, Xv, Zv, iVv, θ, β, A)
     return RBE(MF, [sequence, period, formulation], β, θvec0, θ, remlv, se, F, df, Rv, Vv, G, A, H, Xv, Zv, yv, dH, pO, O)
 end
 
@@ -135,12 +153,22 @@ end
 """
     G matrix
 """
-function gmat(σ₁, σ₂, ρ)
+@inline function gmat(σ₁, σ₂, ρ)
     if ρ > 1.0 ρ = 1.0 end
     if σ₁ < 0.0 σ₁ = 1.0e-6 end
     if σ₂ < 0.0 σ₂ = 1.0e-6 end
     cov = sqrt(σ₁ * σ₂) * ρ
     return [σ₁ cov; cov σ₂]
+end
+@inline function gmat!(G::Matrix{Float64}, σ₁::Float64, σ₂::Float64, ρ::Float64)
+    if ρ > 1.0 ρ = 1.0 end
+    if σ₁ < 0.0 σ₁ = 1.0e-6 end
+    if σ₂ < 0.0 σ₂ = 1.0e-6 end
+    cov = sqrt(σ₁ * σ₂) * ρ
+    G[1, 1] = σ₁
+    G[2, 2] = σ₂
+    G[1, 2] = G[2, 1] = cov
+    return
 end
 
 """
@@ -151,30 +179,22 @@ end
     if σ[2] < 0.0 σ[2] = 1.0e-6 end
     return Matrix(Diagonal((Z*σ)[:,1]))
 end
-
-#=
-function rmat(σ₁, σ₂, Z)
-    if σ₁ < 0.0 σ₁ = 1.0e-6 end
-    if σ₂ < 0.0 σ₂ = 1.0e-6 end
-    M = zeros(4, 4)
-    for i = 1:4
-        M[i, i] = Z[i,:]'*[σ₁, σ₂]
-    end
-    return M
-end
-=#
-#=
-function rmat2(σ, Z)
+@inline function rmat!(R::Matrix{Float64}, σ::Array{Float64, 1}, Z::Matrix{Float64})
     if σ[1] < 0.0 σ[1] = 1.0e-6 end
     if σ[2] < 0.0 σ[2] = 1.0e-6 end
-    return Matrix(Diagonal((Z*σ)[:,1]))
+    copyto!(R, Matrix(Diagonal((Z*σ)[:,1])))
+    return
 end
-=#
+
 """
     Return variance-covariance matrix V
 """
-function cov(G, R, Z)
+@inline function cov(G, R, Z)
     V  = Z*G*Z' + R
+end
+@inline function cov!(V::Matrix{Float64}, G::Matrix{Float64}, R::Matrix{Float64}, Z::Matrix{Float64})
+    copyto!(V, Z*G*Z' + R)
+    return
 end
 function rmatvec(σ₁, σ₂, Zvec)
     n  = length(Zvec)
@@ -193,14 +213,22 @@ function vmatvec(Zvec, G, Rvec)
     end
     return Va
 end
-function cmat(Xv, Zv, θ)
+function matvecz!(M, Zv)
+    n  = length(Zv)
+    for i = 1:n
+        M[i] = zeros(size(Zv[i])[1], size(Zv[i])[1])
+    end
+    return
+end
+function cmat(Xv, Zv, iVv, θ)::Array{Float64, 2}
     p = size(Xv[1])[2]
     C = zeros(p,p)
+    #=
     G   = gmat(θ[3], θ[4], θ[5])
     Rv  = rmatvec(θ[1], θ[2], Zv)
     Vv  = vmatvec(Zv, G, Rv)
     iVv = inv.(Vv)
-
+    =#
     for i=1:length(Xv)
         C = C + Xv[i]'*iVv[i]*Xv[i]
     end
@@ -216,6 +244,16 @@ function βcoef(yv, X, Xv, iVv)
         β = β .+ Xv[i]'*iVv[i]*yv[i]
     end
     return inv(A)*β
+end
+function βcoef!(p::Int, n::Int, yv::Array{Array{Float64, 1}, 1}, Xv::Array{Array{Float64, 2}, 1}, iVv::Array{Array{Float64, 2}, 1}, β::Array{Float64, 1})
+    A = zeros(p,p)
+    β0 = zeros(p)
+    for i = 1:n
+        A = A + (Xv[i]'*iVv[i]*Xv[i])
+        β0 = β0 .+ Xv[i]'*iVv[i]*yv[i]
+    end
+    copyto!(β, inv(A)*β0)
+    return
 end
 #=
 function reml(yv, Zv, X, Xv, θvec)
@@ -247,7 +285,7 @@ function reml(yv, Zv, p, Xv, θvec, β)
     n = length(yv)
     N = sum(length.(yv))
     G = gmat(θvec[3], θvec[4], θvec[5])
-    c  = (N-p)/2*log(2π)
+    c  = (N-p)/2*LOG2PI
     θ1 = 0
     θ2 = 0
     θ3 = 0
@@ -269,7 +307,6 @@ function lcgf(L, Xv, Zv, θ)
     p   = size(Xv[1])[2]
     C   = zeros(p,p)
     G   = gmat(θ[3], θ[4], θ[5])
-
     for i=1:length(Xv)
         R   = rmat([θ[1], θ[2]], Zv[i])
         iV  = inv(cov(G, R, Zv[i]))
@@ -277,8 +314,8 @@ function lcgf(L, Xv, Zv, θ)
     end
     return (L*inv(C)*L')[1]
 end
-function ctrst(p, Xv, Zv, θ, β, A)
-    C     = cmat(Xv, Zv, θ)
+function ctrst(p, Xv, Zv, iVv, θ, β, A)
+    C     = cmat(Xv, Zv, iVv, θ)
     se    = Array{Float64, 1}(undef, p)
     F     = Array{Float64, 1}(undef, p)
     df    = Array{Float64, 1}(undef, p)
@@ -307,12 +344,12 @@ end
 
 
 #-------------------------------------------------------------------------------
-function reml2(yv, Zv, p, Xv, θvec)
+function reml2b!(yv, Zv, p, Xv, G, Rv, Vv, iVv, θvec, β)
     n = length(yv)
     N = sum(length.(yv))
     #This solution with θvec[5] is not the best
     #if θvec[5] > 1.0 θvec[5] = 1.0 end
-    G = gmat(θvec[3], θvec[4], θvec[5])
+    gmat!(G, θvec[3], θvec[4], θvec[5])
     c  = (N-p)*LOG2PI #log(2π)
     θ1 = 0
     θ2 = 0
@@ -320,49 +357,44 @@ function reml2(yv, Zv, p, Xv, θvec)
     iV   = nothing
     θ2m  = zeros(p,p)
     βm   = zeros(p)
-    iVv = Array{Array{Float64,2}, 1}(undef, n)
-
+    #iVv = Array{Array{Float64,2}, 1}(undef, n)
     for i = 1:n
-        R   = rmat([θvec[1], θvec[2]], Zv[i])
-        V   = cov(G, R, Zv[i])
-        iV  = inv(V)
-        iVv[i] = iV
-        θ1  += logdet(V)
-        tm   = Xv[i]'*iV    #Temp matrix for Xv[i]'*iV*Xv[i] and Xv[i]'*iV*yv[i] calc
+        rmat!(Rv[i], [θvec[1], θvec[2]], Zv[i])
+        cov!(Vv[i], G, Rv[i], Zv[i])
+        copyto!(iVv[i], inv(Vv[i]))
+        θ1  += logdet(Vv[i])
+        tm   = Xv[i]'*iVv[i]    #Temp matrix for Xv[i]'*iV*Xv[i] and Xv[i]'*iV*yv[i] calc
         θ2m += tm*Xv[i]
         βm  += tm*yv[i]
     end
-    β        = inv(θ2m)*βm
-
+    copyto!(β, inv(θ2m)*βm)
     for i = 1:n
         r    = yv[i]-Xv[i]*β
         θ3  += r'*iVv[i]*r
     end
-
-    #=
-    θ3 = @sync  @distributed  (+) for i = 1:n
-        r    = yv[i]-Xv[i]*β
-        r'*iV*r
-    end
-    =#
     θ2       = logdet(θ2m)
     return   -(θ1 + θ2 + θ3 + c)
 end
-#=
-function βcoef2(yv, p, Xv, Zv, θvec)
-    n   = length(yv)
-    A   = zeros(p,p)
-    β   = zeros(p)
-    G   = gmat(θvec[3], θvec[4], θvec[5])
+function reml2!(yv::S, Zv::T, p::Int, n::Int, N::Int, Xv::T, G::Array{Float64, 2}, Rv::T, Vv::T, iVv::T, θvec::Array{Float64, 1}, β::Array{Float64, 1})::Float64 where T <: Array{Array{Float64, 2}, 1} where S <: Array{Array{Float64, 1}, 1}
+
+    gmat!(G, θvec[3], θvec[4], θvec[5])
+    c  = (N-p)*LOG2PI
+    θ1 = 0
+    θ2 = 0
+    θ3 = 0
+    θ2m  = zeros(p,p)
     for i = 1:n
-        R   = rmat([θvec[1], θvec[2]], Zv[i])
-        iV = inv(cov(G, R, Zv[i]))
-        A = A + (Xv[i]'*iV*Xv[i])
-        β = β .+ Xv[i]'*iV*yv[i]
+        rmat!(Rv[i], [θvec[1], θvec[2]], Zv[i])
+        cov!(Vv[i], G, Rv[i], Zv[i])
+        copyto!(iVv[i], inv(Vv[i]))
+        θ1  += logdet(Vv[i])
+        θ2m += Xv[i]'*iVv[i]*Xv[i]
+        r    = yv[i]-Xv[i]*β
+        θ3  += r'*iVv[i]*r
     end
-    return inv(A)*β
+    θ2       = logdet(θ2m)
+    return   -(θ1 + θ2 + θ3 + c)
 end
-=#
 function initvar(df, dv, fac, sbj)
     u  = unique(df[:, sbj])
     f  = unique(df[:, fac])

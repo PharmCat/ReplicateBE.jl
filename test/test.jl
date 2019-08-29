e29275cfea849472390e4c06c1a17f4a114e41d2
PharmCat <v.s.arnautov@yandex.ru>
2019-08-29 18:59:14 +0300

    Test g_tol = 1e-10

--- test/test.jl
+++ test/test.jl
@@ -9,7 +9,7 @@ include("testdata.jl")
 
 @testset "  Basic mixed model test                        " begin
     df = CSV.read(IOBuffer(minibe)) |> DataFrame
-    be = ReplicateBE.rbe(df, dvar = :var, subject = :subject, formulation = :formulation, period = :period, sequence = :sequence);
+    be = ReplicateBE.rbe(df, dvar = :var, subject = :subject, formulation = :formulation, period = :period, sequence = :sequence, g_tol = 1e-10);
     @test be.β[6]  ≈  -0.0791666 atol=1E-5
     @test be.se[6] ≈   0.09037378448083119 atol=1E-5
     @test be.reml  ≈  10.065238638105903 atol=1E-5
@@ -20,7 +20,7 @@ end
     #SE 0.04650
     #DF 208
     df = CSV.read(IOBuffer(be4)) |> DataFrame
-    be = ReplicateBE.rbe(df, dvar = :var1, subject = :subject, formulation = :formulation, period = :period, sequence = :sequence);
+    be = ReplicateBE.rbe(df, dvar = :var1, subject = :subject, formulation = :formulation, period = :period, sequence = :sequence, g_tol = 1e-10);
     ci = ReplicateBE.confint(be, 0.1, expci = true, inv = true)
     @test be.reml  ≈  530.1445137281626  atol=1E-5
     @test be.se[6] ≈    0.04650123700721 atol=1E-5
@@ -34,7 +34,7 @@ end
 @testset "  #5 Pub Bioequivalence Dataset                 " begin
     #REML 321.44995530 - SAS STOP!
     df = CSV.read(IOBuffer(be5)) |> DataFrame
-    be = ReplicateBE.rbe(df, dvar = :var1, subject = :subject, formulation = :formulation, period = :period, sequence = :sequence);
+    be = ReplicateBE.rbe(df, dvar = :var1, subject = :subject, formulation = :formulation, period = :period, sequence = :sequence, g_tol = 1e-10);
     ci = ReplicateBE.confint(be, 0.1, expci = true, inv = true)
     @test be.reml  ≈  314.2217688405106 atol=1E-5
     @test ci[5][1] ≈    1.1875472284034538 atol=1E-5
@@ -49,7 +49,7 @@ end
     #DF 62
     #F 2.40
     df = CSV.read(IOBuffer(be6)) |> DataFrame
-    be = ReplicateBE.rbe(df, dvar = :var1, subject = :subject, formulation = :formulation, period = :period, sequence = :sequence);
+    be = ReplicateBE.rbe(df, dvar = :var1, subject = :subject, formulation = :formulation, period = :period, sequence = :sequence, g_tol = 1e-10);
     ci = ReplicateBE.confint(be, 0.1, expci = true, inv = true)
     @test be.reml  ≈  329.25749377843033 atol=1E-5
     @test be.f[6]  ≈  2.399661661708039 atol=1E-5

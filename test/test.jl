511767e0e287b278a08aa938a39b1d62be911b01
PharmCat <v.s.arnautov@yandex.ru>
2019-10-07 22:42:06 +0300

    using CSV fix

--- test/test.jl
+++ test/test.jl
@@ -3,7 +3,7 @@
 # Copyright © 2019 Vladimir Arnautov aka PharmCat <mail@pharmcat.net>
 # Licence: GNU General Public License v3.0
 
-using Test, DataFrames
+using Test, CSV, DataFrames
 
 include("testdata.jl")
 
